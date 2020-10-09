7e8b45c5dff871435d3332e864b3c7c6337e9513
shazia budhwani <mohtashim.siddiqui74@outlook.com>
2020-10-09 11:16:14 -0500

    Avio

---
+++ AvioProWeb/AvioProWeb/Controllers/CategoryController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AvioProWeb.Models;
+
+namespace AvioProWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index(string Params)
+        { 
+            if(Params == "Coatings")
+            {
+            return View("COATINGS");
+            }
+            if (Params == "promoters")
+            {
+                return View("AdhesionPromoters");
+            }
+            if (Params == "Fluids")
+            {
+                return View("AEROSHELL_Fluids");
+            }
+            if (Params == "Lubricants")
+            {
+                return View("Lubricants");
+            }
+            if (Params == "Corrosion")
+            {
+                return View("CorrosionInhibitors");
+            }
+            if (Params == "GREASE")
+            {
+                return View("AeroShell_Grease");
+            }
+            if (Params == "Cleaners")
+            {
+                return View("Electronic_Cleaners");
+            }
+            if (Params == "Tapes")
+            {
+                return View("AerospaceTapes");
+            }
+            if (Params == "ENGINE OIL")
+            {
+                return View("EngineOil");
+            }
+            if (Params == "TURBINE OIL")
+            {
+                return View("COATINGS");
+            }
+            
+            return View("COATINGS");
+        }
+        public ActionResult AddCart(string PartNumber, string Brand, string Name, string Units)
+        {
+            Cart cart = new Cart
+            {
+
+                PartNumber = PartNumber,
+                Brand = Brand,
+                Name = Name,
+                Units = Units,
+               
+            };
+            if (Session["cart"] == null)
+            {
+                List<Cart> li = new List<Cart>();
+                Session["cart"] = li;
+                ViewBag.cart = li.Count();
+                Session["count"] = 1;
+                return View("MyCart", (List<Cart>)Session["cart"]);
+            }
+            else
+            {
+                List<Cart> li = (List<Cart>)Session["cart"];
+                li.Add(cart);//newly entered item
+                Session["cart"] = li;
+                ViewBag.cart = li.Count();
+                Session["count"] = Convert.ToInt32(Session["count"]) + 1;
+                return View("MyCart", (List<Cart>)Session["cart"]);
+
+            }
+            
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Category/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AvioProWeb/AvioProWeb/Controllers/ProductController.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.Web.Mvc;
+
+namespace AvioProWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index(string Param)
+        {
+            if (Param == "3MScotch1252")
+            {
+                return View("coating_3MScotch1252");
+            }
+            if (Param == "3MScotch776")
+            {
+                return View("coating_3MScotch776");
+            }
+            if (Param == "3MScotch5816")
+            {
+                return View("coating_3MScotch5816");
+            }
+            if (Param == "3MScotch1137")
+            {
+                return View("coating_3MScotch1137");
+            }
+            if (Param == "3MScotch776SR")
+            {
+                return View("coating_3MScotch776SR");
+            }
+            //Adhersion Promoters & Primers
+            if (Param == "3MPromoters5000")
+            {
+                return View("Promoters_3MPromoters5000");
+            }
+            if (Param == "3MPromoters5000AS")
+            {
+                return View("Promoters_3MPromoters5000AS");
+            }
+            if (Param == "3MPromoters131")
+            {
+                return View("Promoters_3MPromoters131");
+            }
+            if (Param == "3MPromoters86A")
+            {
+                return View("Promoters_3MPromoters86A");
+            }
+            if (Param == "3MPromoters132")
+            {
+                return View("Promoters_3MPromoters132");
+            }
+            if (Param == "3MPromoters130")
+            {
+                return View("Promoters_3MPromoters130");
+            }
+            if (Param == "3MPromoters1945")
+            {
+                return View("Promoters_3MPromoters1945");
+            }
+            if (Param == "3MPromoters137")
+            {
+                return View("Promoters_3MPromoters137");
+            }
+            if (Param == "3MPromoters160")
+            {
+                return View("Promoters_3MPromoters160");
+            }
+            if (Param == "3MPromoters5005")
+            {
+                return View("Promoters_3MPromoters5005");
+            }
+            if (Param == "3MPromoters3960")
+            {
+                return View("Promoters_3MPromoters3960");
+            }
+            if (Param == "3MPromoters2333")
+            {
+                return View("Promoters_3MPromoters2333");
+            }
+            if (Param == "3MPromoters2174")
+            {
+                return View("Promoters_3MPromoters2174");
+            }
+            if (Param == "3MPromoters3924")
+            {
+                return View("Promoters_3MPromoters3924");
+            }
+            if (Param == "3MPromoters3901")
+            {
+                return View("Promoters_3MPromoters3901");
+            }
+            if (Param == "3MPromoters1593")
+            {
+                return View("Promoters_3MPromoters1593");
+            }
+            if (Param == "3MPromoters1660")
+            {
+                return View("Promoters_3MPromoters1660");
+            }
+            if (Param == "3MPromoters3917")
+            {
+                return View("Promoters_3MPromoters3917");
+            }
+            if (Param == "3MPromoters1290")
+            {
+                return View("Promoters_3MPromoters1290");
+            }
+            //Fluids
+            if (Param == "AeroShellFluid2")
+            {
+                return View("Aero_Fluid2");
+            }
+            if (Param == "AeroShellCompound7")
+            {
+                return View("Aero_Compound7");
+            }
+            if (Param == "AeroShellFluid2XN")
+            {
+                return View("Aero_Fluid2XN");
+            }
+            if (Param == "AeroShellFluid3")
+            {
+                return View("Aero_Fluid3");
+            }
+            if (Param == "AeroShellFluid5MA")
+            {
+                return View("Aero_Fluid5MA");
+            }
+            if (Param == "AeroShellFluid12")
+            {
+                return View("Aero_Fluid12");
+            }
+            if (Param == "AeroShellFluid31")
+            {
+                return View("Aero_Fluid31");
+            }
+            if (Param == "AeroShellFluid61")
+            {
+                return View("Aero_Fluid61");
+            }
+            if (Param == "AeroShellFluid41")
+            {
+                return View("Aero_Fluid41");
+            }
+            if (Param == "AeroShellFluid602")
+            {
+                return View("Aero_Fluid602");
+            }
+            if (Param == "AeroShellFluid8350")
+            {
+                return View("Aero_Fluid8350");
+            }
+            if (Param == "AeroShellGearFluid")
+            {
+                return View("Aero_GearFluid");
+            }
+            if (Param == "AeroShelSmokeOil")
+            {
+                return View("Aero_SmokeOil");
+            }
+            //Lubricants
+            if (Param == "LPSChainMate")
+            {
+                return View("LPS_ChainMate");
+            }
+            if (Param == "LPSPTFE")
+            {
+                return View("LPS_PTFE");
+            }
+            if (Param == "LPSGreaseLess")
+            {
+                return View("LPS_GreaseLess");
+            }
+            if (Param == "LPSHeavyDuty")
+            {
+                return View("LPS_HeavyDuty");
+            }
+            if (Param == "LPSPenetrant")
+            {
+                return View("LPS_Penetrant");
+            }
+
+
+            return View("coating_3mscotch");
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
