a173f749e7a0fad2cf71c223c6e9f981e82ffc53
mars <mohtashim.siddiqui74@outlook.com>
2020-10-17 17:52:03 +0500

    Finalized add to cart in 3M Tapes

---
+++ AvioProWeb/AvioProWeb/Controllers/brandController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvioProWeb.Controllers    
+{
+    public class brandController : Controller
+    {
+        // GET: brand
+        public ActionResult Index(string Params)
+        {
+            if (Params == "3m")
+            {
+                return View("ThreeM");
+            }
+            if (Params == "LPS")
+            {
+                return View("LPS");
+            }
+            if (Params == "AEROSHELL")
+            {
+                return View("AEROSHELL");
+            }
+
+
+            return View("ThreeM");
+        }
+    }
+}
